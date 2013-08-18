4af9b7294dac1183e3e9485c398868c0b16773ca
cyawman <cyawman@192.168.2.21>
2013-08-18 17:41:29 -0400

    Navigation Mods

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,13 @@
 var SweetCakes = {
     ready : (function($){
-        
+        $('.site-navigation .navigation li').hover(function() {
+            $(this).find('ul').show();
+        },
+        function() {
+          $(this).find('ul').hide();
+        });
     })
-}
+};
 
 $(document).ready(function(){
     SweetCakes.ready();
