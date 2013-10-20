b095b83c8fd0cdb04aed33aaff1fcda41a76130f
Yawman, Chris <cyawman@gmail.com>
2013-10-19 20:04:41 -0400

    Add Carousel and Modal popup

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,26 @@
-var SweetCakes = {
-    ready : (function($){
-        
-    })
-};
+var SweetCakes = (function($){
+    
+    var bootstrap = function(){
+        $('.thumbnail-popup-trigger').on('click', handleThumbnailPopup);
+        $("#homepage-carousel").carousel();
+    }
+    
+    var handleThumbnailPopup = function(event){
+        event.preventDefault();
+        var $target = $(event.target);
+        $('#thumbnail-popup .modal-body img').remove();
+        $('#thumbnail-popup .modal-title').text($target.attr('alt'));
+        $('#thumbnail-popup .modal-body').append($('<img src="'+$target.data('fullimage')+'"></img>'));
+        $('#thumbnail-popup').modal();
+    }
+    
+    return {
+        ready : (function($){
+            bootstrap();
+        })
+    }
+    
+})(jQuery);
 
 $(document).ready(function(){
     SweetCakes.ready();
